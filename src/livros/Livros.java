8e2b2d3b94fde5fad5cdee076fcc7412cf4167be
Stheven <sthevenalvess@gmail.com>
2024-07-10 15:25:30 -0300

    feat: entidade livro

---
+++ src/livros/Livros.java
@@ -0,0 +1,51 @@
+package livros;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Livros {
+    private String autor;
+    private String titulo;
+    private LocalDate datePublicacao;
+
+    public Livros() {
+    }
+
+    public Livros(String autor, String titulo, LocalDate datePublicacao) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.datePublicacao = datePublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDate getDatePublicacao() {
+        return datePublicacao;
+    }
+
+    public void setDatePublicacao(LocalDate datePublicacao) {
+        this.datePublicacao = datePublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", Data='" + datePublicacao +"'"+
+                '}'+"\n";
+    }
+}
